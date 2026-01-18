3d0391e05061ed3b71699f568596c42d2347204c
alper <alperclbi@hotmail.com>
2026-01-18 15:54:48 +0300

    kartlar eklendi ürün görselleri geldi. detay sayfası yapıldı.

--- Project_Development/Controllers/HomeController.cs
+++ Project_Development/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Project_Development.Models;
@@ -19,8 +20,28 @@ namespace Project_Development.Controllers
         public IActionResult Index()
         {
             var categories = _projectContext.Categories.ToList();
-            var products = _projectContext.Products.ToList();
+            var products = _projectContext.Products
+                .Include(x=> x.ProductImages)
+                .ToList();
             return View((categories,products));
+        }
+
+        public IActionResult ProductDetail(int id)
+        {
+            var product = _projectContext.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .Include(p => p.ProductColors).ThenInclude(pc => pc.Color)
+                .Include(p => p.ProductSizes).ThenInclude(ps => ps.Size)
+                .Include(p => p.ProductProperties)
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         public IActionResult Privacy()
