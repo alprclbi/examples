16bf36d91e264bd217617bafee8b3805377403f1
alper <alperclbi@hotmail.com>
2026-02-23 15:07:10 +0300

    IUnıtOfWork tamamlandı.

---
+++ StudentDemo/StudentDemo.Core/Interfaces/IUnıtOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo.Core.Interfaces
+
+/// <summary>
+/// IunitOfWork - Unıt Of Work Pattern Interface
+/// 
+/// Amaç: Birden fazla repository işlemini tek bir transaction'da yönetmek
+/// 
+/// Örnek Senaryo:
+///  - Bir yazar eklenirken aynı anda kitaplerı da eklenmeli
+///  - İşlemlerden biri başarısız olursa hepsi geri alınmalı (rollback)
+///  
+/// SOLID: Single Responsibility - Sadece transaction yönetimi
+/// </summary>
+{
+    public interface IUnıtOfWork : IDisposable
+    {
+        // Repository'lere erişim
+        IBookRepository Books { get; }
+
+        IAuthorRepository Authors { get; }
+
+        // Tüm değişiklikleri kaydet
+        Task<int> SaveChangesAsync();
+
+        // Transaction başlat
+        Task BeginTransactionAsync();
+
+        // Transaction onayla
+        Task CommitTransactionAsync();
+
+        // Transaction geri al
+        Task RollbackTransactionAsync();
+    }
+}
